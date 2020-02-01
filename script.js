d9255e368529468c29ecf076691292a70154e4b9
Sayan Roy <sayanroy058@gmail.com>
2020-02-01 18:06:32 +0530

    Website

--- script.js
+++
@@ -1,9 +0,0 @@
-// Get the menu button
-const burger = document.querySelector(".burger")
-
-//Event listner for click
-burger.addEventListener("click",()=>{
-  var ul= document.querySelector("body .Body header nav ul")
-  console.log("Hello")
-    ul.classList.toggle("active")
-});
